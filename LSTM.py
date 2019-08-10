3b88ae45ac306ba9884757e2a89f10d66de667ad
Raj M Shukla <rshukla@0562118xb1nx1l.rd.unr.edu>
2019-08-09 18:54:35 -0700

    tensor board callback

--- LSTM.py
+++ LSTM.py
@@ -1,9 +1,10 @@
+from tensorflow import keras
 import numpy as np
 import random
 from scipy import stats
 from scipy.stats import kurtosis, skew
 import math
-from keras.callbacks import History
+from keras.callbacks import History, TensorBoard
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
 from keras.layers import LSTM, Dropout
@@ -12,8 +13,6 @@ import json
 import csv
 
 np.random.seed(7)
-#X = process_data.X
-#Y = process_data.Y
 X = []
 Y = []
 with open("input_data.csv", "r") as input_file:
@@ -27,26 +26,14 @@ with open("output_data.csv", "r") as output_file:
     for row in reader:
         Y = [float(i) for i in row]
 
-
-
 X, Y = np.array(X), np.array(Y)
-
 X = np.reshape(X, (X.shape[0], X.shape[1], 1))
-
 Y = np.reshape(Y, (X.shape[0], 1))
-
 X_train = X[0:2000, :, :]
 Y_train = Y[0:2000]
-
 X_test = X[2000:, :, :]
 Y_test = Y[2000:]
-
-
-
-
-
-print(X.shape)
-print(Y.shape)       
+    
 
 layers = 4
 epochs = 1000
@@ -67,30 +54,15 @@ model.add(Dense(units = 1))
 
 model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics=['mae', 'acc'])
 
-history = model.fit(X_train, Y_train, batch_size= 256, verbose=1, epochs=epochs)
-file_name = "model_layers_"+ str(layers)+ "_" + "epochs_" + str(epochs)
-
-print(file_name)
+tensor_board = TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
+history = model.fit(X_train, Y_train, batch_size= 256, verbose=1, epochs=epochs,
+                  callbacks=[tensor_board])
+#print(file_name)
 
 model.save(file_name + '.h5')
 model.summary()
 
-with open(file_name +  '.json', 'w') as f:
-    f.write(model.to_json())
-    
-with open( file_name + '_history.json', 'w') as f:
-    json.dump(history.history, f)
+score = model.evaluate(X_test, Y_test)
+print (score)
 
-
-prediction_train = model.predict(X_train).flatten()
-error_train = np.mean(np.abs(prediction_train - Y_train))
-
-prediction_test = model.predict(X_test).flatten()
-error_test = np.mean(np.abs(prediction_test - Y_test))
-
-
-
-
-
-   
         
--- ResNet.py
+++ ResNet.py
@@ -4,7 +4,7 @@ import random
 from scipy import stats
 from scipy.stats import kurtosis, skew
 import math
-from keras.callbacks import History
+from keras.callbacks import History, TensorBoard
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
 from keras.layers import LSTM, Dropout
@@ -13,6 +13,30 @@ import json
 import csv
 
 np.random.seed(813306)
+
+X = []
+Y = []
+with open("input_data.csv", "r") as input_file:
+    reader = csv.reader(input_file)
+    for row in reader:
+        x = [float(i) for i in row]
+        X.append(x)
+        
+with open("output_data.csv", "r") as output_file:
+    reader = csv.reader(output_file)
+    for row in reader:
+        Y = [float(i) for i in row]
+
+
+
+X, Y = np.array(X), np.array(Y)
+X = np.reshape(X, (X.shape[0], X.shape[1], 1, 1))
+Y = np.reshape(Y, (X.shape[0], 1))
+X_train = X[0:2000, :, :, :]
+Y_train = Y[0:2000]
+X_test = X[2000:, :, :, :]
+Y_test = Y[2000:]
+
  
 def build_resnet(input_shape, n_feature_maps):
     print ('build conv_x')
@@ -97,65 +121,29 @@ def build_resnet(input_shape, n_feature_maps):
     return x, out
  
        
-#X = process_data.X
-#Y = process_data.Y
-X = []
-Y = []
-with open("input_data.csv", "r") as input_file:
-    reader = csv.reader(input_file)
-    for row in reader:
-        x = [float(i) for i in row]
-        X.append(x)
-        
-with open("output_data.csv", "r") as output_file:
-    reader = csv.reader(output_file)
-    for row in reader:
-        Y = [float(i) for i in row]
 
-
-
-X, Y = np.array(X), np.array(Y)
-
-X = np.reshape(X, (X.shape[0], X.shape[1], 1, 1))
-
-Y = np.reshape(Y, (X.shape[0], 1))
-
-X_train = X[0:2000, :, :, :]
-Y_train = Y[0:2000]
-
-X_test = X[2000:, :, :, :]
-Y_test = Y[2000:]
 
       
 layers = 4
-epochs = 1500
+epochs = 1
 file_name = "ResNet_layers_"+ str(layers)+ "_" + "epochs_" + str(epochs)
      
 x , y = build_resnet(X_train.shape[1:], 64)
 model = keras.models.Model(inputs=x, outputs=y)
-model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics=['mae', 'acc'])
-#reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=50, min_lr=0.0001) 
 
-history = model.fit(X_train, Y_train, batch_size= 256, verbose=1, epochs=epochs)
-
-
-
-print(file_name)
+model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics=['mae'])
+tensor_board = TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
+history = model.fit(X_train, Y_train, batch_size= 256, verbose=1, epochs=epochs,
+                  callbacks=[tensor_board])
+#print(file_name)
 
 model.save(file_name + '.h5')
 model.summary()
 
-with open(file_name +  '.json', 'w') as f:
-    f.write(model.to_json())
-    
-with open( file_name + '_history.json', 'w') as f:
-    json.dump(history.history, f)
+score = model.evaluate(X_test, Y_test)
+print (score)
 
 
-prediction_train = model.predict(X_train).flatten()
-error_train = np.mean(np.abs(prediction_train - Y_train))
 
-prediction_test = model.predict(X_test).flatten()
-error_test = np.mean(np.abs(prediction_test - Y_test))
 
 
